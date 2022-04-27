881f5b1fcafa5529966297b2a23e8831e1a8c61a
VictoKu1 <kushnir.victor@gmail.com>
2022-04-27 22:37:47 +0300

    .

--- sattack.sh
+++ sattack.sh
@@ -58,7 +58,7 @@ fi
 
 # Perform a WLAN scan for 1 minute and print the BSSID and channel
 echo "Scanning for WLANs..."
-iwlist $interface scan | grep -i bssid | awk '{print $1, $4}' 
+iwlist $interface scan | grep -i bssid | awk '{print $0 ,$1,$2,$3,$4,$5,$6,$7, $8}' 
 
 
 
